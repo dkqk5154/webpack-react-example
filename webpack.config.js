a3ae67868711a9466c9befadd8be8d047a5ff827
dkqk5154 <dkqk5154@gmail.com>
2022-01-05 16:59:46 +0900

    Remove dummy module

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,6 @@ module.exports = {
                             "@babel/preset-env",
                             "@babel/preset-react",
                         ],
-                        // plugins: ["@babel/plugin-transform-react-jsx-compat"]
                     }
                 },
             },
