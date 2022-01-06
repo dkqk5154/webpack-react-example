763448e722658e85a12541885158700a33d8ef01
dkqk5154 <dkqk5154@gmail.com>
2022-01-06 10:04:27 +0900

    Add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import Button from "./components/common/Button";
 
 const Styled = {
   Wrapper: styled.div`
     display: flex;
+    gap: 12px;
+    flex-direction: column;
     border: 1px solid black;
     padding: 1rem;
     align-items: center;
@@ -11,6 +14,20 @@ const Styled = {
   `,
 };
 
-const App = () => <Styled.Wrapper>Hello, webpack!</Styled.Wrapper>;
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClickButton = (e) => {
+    e.preventDefault();
+    setCount((prevState) => prevState + 1);
+  };
+
+  return (
+    <Styled.Wrapper>
+      <div>Hello, webpack! </div>
+      <Button onClick={handleClickButton}>클릭 버튼 카운터 {count}</Button>
+    </Styled.Wrapper>
+  );
+};
 
 export default App;
