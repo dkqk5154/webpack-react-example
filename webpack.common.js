94bfb6d9bdab0cab2b3d1b2515ab95de2bee80a9
dkqk5154 <dkqk5154@gmail.com>
2022-01-06 11:48:33 +0900

    Fix build ouput folder name

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,11 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "bundle.[hash].js",
+        path: path.resolve(__dirname, 'build'),
     },
     resolve: {
         extensions: ['', '.js', '.jsx']
